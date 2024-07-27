73dc8b960cad6bdb46f7cf16921a422fb021c839
wheatfox <enkerewpo@hotmail.com>
2024-07-27 11:36:54 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mit-6.828
 
+https://pdos.csail.mit.edu/6.828/2018/overview.html
+
 wheatfox
--- lab1/lib/printfmt.c
+++ lab1/lib/printfmt.c
@@ -206,7 +206,7 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			putch('0', putdat);
+			// putch('0', putdat);
 			num = getuint(&ap, lflag);
 			base = 8;
 			printnum(putch, putdat, num, base, width, padc);
